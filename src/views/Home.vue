fe99c998fc7826790c45802f8a79c4a90a84fe40
Andrea <castenetto.andrea@gmail.com>
2021-04-24 12:24:38 +0200

    4.4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,6 @@
 
     <main class="words" v-if="words">
       <div class="word" v-bind:key="key" v-for="singleWord, key in words" :style="{backgroundColor: singleWord.color}">
-         {{singleWord}}
        <div class="star">
          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-star" viewBox="0 0 18 18">
             <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
@@ -57,23 +56,16 @@ export default {
     return {
       basicWords: ["Ammirazione", "Apprezzamento", "Attenzione", "Bellezza", "Beatitudine", "Calma", "Compassione", "Comprensione", "Collaborazione", "Coraggio", "Creatività", "Audacia", "Decisione", "Distacco", "Determinazione", "Discernimento", "Disciplina", "Energia", "Decisione", "Distacco", "Determinazione", "Discernimento", "Disciplina", "Energia", "Entusiasmo", "Eternità", "Fede", "Libertà", "Amicizia", "Generosità", "Bontà", "Volontà buona", "Gratitudine", "Armonia", "Umorismo", "Inclusività", "Infinito", "Iniziativa", "Integrazione", "Gioia", "Liberazione", "Luce", "Amore", "Ordine", "Pazienza", "Pace", "Costanza", "Positività", "Forza", "Prontezza", "Quiete", "Realtà", "Rinnovamento", "Risolutezza", "Serenità", "Servizio", "Silenzio", "Semplicità", "Sintesi", "Tenacia", "Verità", "Comprensione", "Universalità", "Vitalità", "Interezza", "Volontà", "Saggezza", "Stupore"].sort( () => .5 - Math.random() ), // randomized array folks
       words: [],
-      colors: []
     }
   },
   created: function() {
-    this.colors = randomColor({ luminosity: 'light', hue: 'green', count: this.words.length });
+    let c = randomColor({ luminosity: 'light', hue: 'green', count: this.basicWords.length });
 
-    // why random color return an observer?
-
-    console.log(this.colors);
-
-    let w = [], self = this;
+    let w = [];
     this.basicWords.map(function(el, i, arr) {
-      console.log(self.colors, i)
-      w.push({ word: el, color: self.colors[i], order: 1, starred: false });
+      w.push({ word: el, color: c[i], order: 1, starred: false });
     });
 
-    console.log(w);
     this.words = w;
   },
   methods: {
