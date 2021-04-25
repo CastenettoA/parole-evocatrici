6f5960c6cca6fb947bd1f41e36e857d5d3b20aa5
Andrea <castenetto.andrea@gmail.com>
2021-04-25 11:47:38 +0200

    5.0

---
+++ src/views/Metodi.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="home-view">
+    <nav>
+      <section>
+      <div class="logo">
+        <div class="logo-wrapper" @click="goToHome()">
+          <div class="img">
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#444" class="bi bi-spellcheck" viewBox="0 0 16 16">
+              <path d="M8.217 11.068c1.216 0 1.948-.869 1.948-2.31v-.702c0-1.44-.727-2.305-1.929-2.305-.742 0-1.328.347-1.499.889h-.063V3.983h-1.29V11h1.27v-.791h.064c.21.532.776.86 1.499.86zm-.43-1.025c-.66 0-1.113-.518-1.113-1.28V8.12c0-.825.42-1.343 1.098-1.343.684 0 1.075.518 1.075 1.416v.45c0 .888-.386 1.401-1.06 1.401zm-5.583 1.035c.767 0 1.201-.356 1.406-.737h.059V11h1.216V7.519c0-1.314-.947-1.783-2.11-1.783C1.355 5.736.75 6.42.69 7.27h1.216c.064-.323.313-.552.84-.552.527 0 .864.249.864.771v.464H2.346C1.145 7.953.5 8.568.5 9.496c0 .977.693 1.582 1.704 1.582zm.42-.947c-.44 0-.845-.235-.845-.718 0-.395.269-.684.84-.684h.991v.538c0 .503-.444.864-.986.864zm8.897.567c-.577-.4-.9-1.088-.9-1.983v-.65c0-1.42.894-2.338 2.305-2.338 1.352 0 2.119.82 2.139 1.806h-1.187c-.04-.351-.283-.776-.918-.776-.674 0-1.045.517-1.045 1.328v.625c0 .468.121.834.343 1.067l-.737.92z"/>
+              <path d="M14.469 9.414a.75.75 0 0 1 .117 1.055l-4 5a.75.75 0 0 1-1.116.061l-2.5-2.5a.75.75 0 1 1 1.06-1.06l1.908 1.907 3.476-4.346a.75.75 0 0 1 1.055-.117z"/>
+            </svg>
+          </div>
+          <div class="title">Parole Evocatrici</div>
+        </div>
+    </div>
+
+      <p class="summary">Breve introduzione alla tecnica della parole evocatrici di <b>Roberto Assagioli</b>.</p>
+      </section>
+    </nav>
+
+    <main class="pratica">
+      <h1>Metodi, Modalità e Campi di applicazione della tecnica delle parole evocatrici</h1>
+      <p>Questa tecnica può essere utilizzata in vari modi e modificata creativamente per adeguarla alle tue esigenze particolari. 
+        Qui sotto trovi i metodi principali estratti dal libro <a href="https://amzn.to/3nmT7JA">L'atto di Volontà</a> di Roberto Assagioli, pagina 62-3.
+      </p>
+
+      <h2>Il cartoncino</h2>
+      <p>Il più semplice è quello di posizionare un cartoncino nel quale è stampata la parola o qualità che vogliamo "risvegliare", e poi posizionarlo
+        in un luogo facilmente visibile e accessibile (la nostra scrivania, una parete, un tavolo, ecc). <br>Per aumentare l'effetto è possibile aumentare
+        il numero dei cartoncini. L'immagine visiva si imprime, anche senza che vi prestiamo attenzione nella nostra psiche (inconsio plastico), e opera in essa.
+      </p>
+      <p>Come variazione, invece di una parola, possiamo scrivere slogan o frasi suggestive che esprimono simbolicamente un messaggio.</p>
+      <p> <a href="https://amzn.to/3nmT7JA">Nel libro</a> di Roberto A. vengono suggerite alcune frasi prese 
+      dalle iscrizioni sugli stemmi di varie famiglie nobili: Ad sidera vultus (Rivolto alle stelle), Pensa al fine, Bien faire et laisser dire (Agire bene e lasciar dire), Semprer vigilans (Sempre vigile), In tutto armonia.</p>
+
+      <h2>L'attenzione cosciente</h2>
+      <p>E' il metodo più efficace consiste nel prestare coscientemente attenzione alla parola scelta, convogliando quindi le proprie energie psicologiche sulla qualità da sviluppare.</p>
+      <p>Ciò può essere fatto in vari modi (segliendone uno o più di uno), dopo esserti messo in uno stato di rilassamento mentale e muscolare:</p>
+      <ol>
+        <li><i>Osservare</i> per qualche minuto la parola (se affiorano dall'inconsio idee o immagini collegate con la parola, lasciarle emergere e poi segnarle).</li>
+        <li><i>Meditare, riflettere</i> sul significato della parola, e poi segnare i risultati di tale meditazione</li>
+        <li>Cercare di <i>sentire</i> la qualità psicologica contenutaa nella parola, lasciatene permeare, se possibile fino ad arrivare ad identificarti con essa.</li>
+        <li>Mentre si osserva la parola, pronunciarla ad alta voce, o mormorala.</li>
+        <li>Scrivere la parola diverse volte.</li>
+      </ol>   
+
+      <p>Possiamo associare a questi metodi immagini visive, uditive e motorie, aumentando così l'efficacia dell'esercizio.<br>
+      Ad esempio ascoltando temi, brani musicali collegati con parole e frasi relative oppure movimenti ritrmici, gesti e danze che ricordano il simbolo associato alla parola.
+      </p>
+
+      <h2>Le parole evocatrici al lavoro e in famiglia</h2>
+      <p>Possiamo certamente utilizzare questa tecnica anche nel nostro ambiente di lavoro ed in famiglia. Coinvolgendo le altre persone in esercizi di gruppo 
+        oppure utilizzando l'esercizio del cartoncino, o stramite l'affisione di slogan nei corridoi.</p>
+        <p> Un esempio pratico può essere quello di esporre la parola <i>Creatività</i> in una sala 
+        riunioni, oppure, perchè no, la parola <i>Calma</i> in ambienti dove spesso la tensione fra i partecipanti si fa sentire. Un altra idea è 
+        posizionare grande cartoncino con scritto <i>Serenità</i> nelle aree dedicate al relax, oppure arredare la stanza con quadri che evocano l'idea corrispondente.
+      </p>
+
+      <h2>Le parole evocatrici nella società umana</h2>
+      <p>Gli esperti della persuasione ci bombardano ogni giorno con publicità (cartelli di grandi dimensioni, frasi suggestive, 
+        slogan, immagini affascinanti, ripetizione martellante, brani musicali), spesso per creare in noi il desiderio di acquistare un prodotto 
+        non necessario</p>
+      <p>Queste stesse tecniche, che vengono utilizzate così bene per scopi commerciali o politici, potrebbero e dovrebbero venire impiegate per scopi 
+        superiori e benefici nei vari livelli della nostra società umana.</p>
+      <p>Come dice Assagioli, non sarebbe affatto difficile organizzare una proficua <i>pubblicità psico-spirituale</i> per il miglioramento e l'elevazione 
+      individuale e collettiva dell'umanità</p>
+      <p>Personalmente, poco per volta, sto iniziando a regalare dei cartoncini ai miei amici o familiari, spiegando anche la tecnica, e sembrano essere 
+        sempre ben accetti.</p>
+
+      <p>E tu? Hai mai condiviso un <i>cartoncino</i> a un tuo conoscente? Se vuoi puoi condividere questa applicazione e farla conoscere ai tuoi amici.</p>
+
+      <h2>Altri campi di applicazione</h2>
+      <p></p>
+    </main>
+
+    <footer>Puoi sostenere questo progetto con una <a href="https://paypal.me/pools/c/8bFReaiLUk">donazione</a>.<br> Se vuoi inviarmi un feedback fallo via <a href="mailto:castenetto.andrea@gmail.com">email</a>. <br> <a href="#top">Torna su ⬆</a> • <a href="https://castenettoa.com" title="creato da Andrea Castenetto">castenettoa.com</a></footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Metodi',
+  data: function() {
+    return {
+    }
+  },
+  methods: {
+    goToHome: function(w) { 
+      console.log(this.$route);
+      if(this.$route.name != 'Home')
+        this.$router.push({ name: "Home" });
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  .pratica {
+    margin-top: 16em;
+  } 
+</style>
