88bb2a89553570dc605864158bb285036ba1e494
Andrea <castenetto.andrea@gmail.com>
2021-04-28 10:14:55 +0200

    7.3

--- src/views/docs/Leggi.vue
+++ src/views/docs/Leggi.vue
@@ -20,39 +20,53 @@
 
     <main class="pratica">
       <h1>Le leggi psicologiche</h1>
-       <p>Così come il mondo fisico è governato da leggi, anche il nostro funzionamento psichico è governato da altrettante <i>leggi psicologiche</i>.</p>
+       <p>Così come il mondo fisico esistono delle leggi, anche il nostro funzionamento psichico è governato da altrettante <i>leggi psicologiche</i>.</p>
        <p>Conoscere queste leggi significa innanzitutto essere più coscienti dei nostri meccanismi interni e 
          ci consente di utilizzare queste stesse leggi a nostro favore; ad esempio, la <a href="/tecnica">tecnica delle parole evocatrici</a> fa largo uso di queste leggi.</p>
          <p>Verranno ora qui elencate e brevemente descritte queste leggi; se il lettore vuole approfindire 
            trova una più dettagliata spegazione nel libro <a href="https://amzn.to/3nmT7JA">L'atto di volontà di Roberto Assagioli.</a> a partire da pagina 44 (cap. volontà sapiente).</p>
 
         <h3>1° legge: Le immagini o figure mentali e le idee tendono a produrre le condizioni fisiche e gli atti esterni ad esse corrispondenti.</h3>
-        <p>Così come il mondo fisico è governato da leggi, anche il nostro funzionamento psichico è governato da altrettaCosì come il mondo fisico è governato da leggi, anche il nostro funzionamento psichico è governato da altretta</p>
+        <p>Ogni idea è un azione allo stato latente e questa legge è provata ampiamente dagli effetti dell'influsso ipnotico e, nell'ordinario stato di veglia
+           dalla suggestione, dall'autosuggestione, e dalla suggestione di massa. E' alla base degli effetti psicosomatici sia patologici che terapeutici ed è sfruttata 
+           largamente da pubblicitari, uomini politici e da tutti i "persuasori".</p>
+           <p>La volontà quindi, può e deve utilizzare questa legge, mobilitando le energie dell'immaginazione e del pensiero utilizzandole per raggiungere i nostri scopi. (con le parole evocatrici facciamo proprio questo, dirigiamo tramite la volontà le nostre forze psicologiche coscientemente sulla qualità che vogliamo sviluppare)</p>
    
    
         <h3>2° legge: Gli atteggiamenti, i movimenti e le azioni tendono ad evocare le immagini e le idee corrispondenti; queste, a loro volta (2° legge) evocano o rendono più intensi le emozioni e i sentimenti.</h3>
-        <p></p>
+        <p>Conoscendo questa legge, con la volontà possiamo muovere il nostro corpo e quindi evocare immagini e idee corrispondenti che poi susciteranno emozioni e sentimenti. Uno dei possibili scopi è quello 
+          evocare e rafforzare stati interiori positivi attraverso movimenti coscienti. (praticare un movimento associato con la parola che vogliamo evocare stimolerà il nostro stato interiore corrispondente)
+        </p>
 
         <h3>3° legge: Le idee e le immagini tendono a suscitare le emozioni ed i sentimenti ad esse corrispondenti.</h3>
-        <p></p>
+        <p>Ad es. praticare la tecnica delle parole evocatrici con la parola <b>Gioia</b> evocherà in noi le emozioni ed i sentimeti ad essa collegati.</p>
+        <p>Sempre tramite la volontà possiamo usare sapientemente questa legge per evocare in noi gli stati interni desiderati.</p>
 
         <h3>4° legge: Le emozioni e le impressioni tendono a suscitare ed a intensificare le idee e le immagini ad esse corrispondenti o collegate.</h3>
-        <p></p>
+        <p>Le emozioni, a loro volta, tendono a intensificare le idee ad esse collegate. Qui assistiamo ad un processo di feedback che può agire positivamente (parole evocatrici) o negativamente (paura di una malattia che genera immagini mentali nefaste)</p>
 
         <h3>5° legge: I bisogno, gli istinti, gli impulsi e i desideri tendono a produrre le immagini, le idee e le emozioni corrispondenti. Immagini ed idee, a loro volta (1° legge) suggeriscono le azioni corrispondenti.</h3>
-        <p></p>
+        <p>E' bene essere consapevoli i nostri impulsi e desideri più ardenti spingono per trovare ragioni o pseudo-ragioni (razionalizzazione) per realizzarsi; è nostra responsabilità 
+          vigilare per individuare possibili illusioni mentali ed emotive nei nostri pensieri. Quindi diventare più consapevoli di dove i nostri istinti, impulsi e desideri vogliono portarci.
+        </p>
 
         <h3>6° legge: L'attenzione, l'interesse, l'affermazione e la ripetizione rafforzano le idee, le immagini e le formazioni psicologiche su cui si accentrano.</h3>
-        <p></p>
+        <p>Prestare attenzione per qualche minuti ad una <b>parola evocatrice</b> significa esattamente questo, potenziare l'immagine mentale e ne aumenta l'efficacia. Tecnica utilizzata molto bene dai pubblicitari e dai "persuasori".</p>
+        <p>E' bene notare che noi possiamo utilizzare anche la 6° legge in senso opposto, ritirando l'attenzione da un contenuto psicologico sgradito, riducendone così l'energia e l'attività.</p>
 
         <h3>7° legge: La ripetizione degli atti intensifica la tendenza a compierli e rende più faile e migliore la loro esecuzione, fino a che si arriva a poterli compiere inconsciamente.</h3>
-        <p></p>
+        <p>Legge che descrive la formazione delle abitudini. Evidenzia la nostra responsabilità nel valutare le nostre abitudini e decidere eventualmente di modificarle o di crearne di nuove.</p>
+        <p>Anche se in parte tendono a limitarci e farci percorrere sentieri già battuti è bene sapere che: "La volontà e l'intelletto possono formare abitudini di pensiero e volontà. Noi siamo responsabili delle nostre abitudini di pensiero e anche quando 
+          agiamo secondoo un'abitudine agiamo liberamente." &mdash; William James</p>
 
         <h3>8° legge: Tutte la varie funzioni, e le loro molteplici combinazioni in complessi e sub-personalità, mettono in moto la realizzazione dei loro scopi al di fuori della nostra coscienza, e indipendentemente da, e perfino contro, la nostra volontà.</h3>
-        <p></p>
+        <p><a href="https://amzn.to/3nmT7JA">Rimando al libro per maggiori dettagli.</a> pag 50.</p>
 
         <h3>9° legge: Gli istinti, gli impulsi, i desideri e le emozioni tendono ad esprimersi ed esigono espessione.</h3>
-        <p></p>
+        <p>Come per la legge 5° anche qui è necessaria un attenta valutazione dei nostri impulsi e desideri (osservandoli da una posizione centrale, disidentificata) al fine di lavorare su un problema pratico: 
+          "Come esprimere in modo adeguato e costruttivo, o almeno non dannoso, tutti questi istinti, impulsi, emozioni, ecc"?
+        </p>
+        <p>Non esprimerli potrebbe significare disturbi psicosomatici, stress psichici e nervosi.</p>
 
         <h3>10° legge: Le energie psichiche si possono esprimere: (1) direttamente (sfogo-catarsi); (2) indirettamente, attraverso un'azione simbolica; (3) con un processo di trasmutazione.</h3>
         <p></p>
