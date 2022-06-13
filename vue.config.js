c867f4f086b8121282d2520c6706c097b6583aeb
castenettoa <castenetto.andrea@gmail.com>
2022-06-13 14:16:05 +0200

    rm error pwa

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
   pwa: {
     name: 'Parole Evocatrici',
     themeColor: '#4DBA87', // slate green
-  }
+  },
+  // workboxOptions: {
+  //   exclude: [/_redirects/]
+  // }
 })
